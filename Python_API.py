07dba3994051bc3da124eb97bf8d74133a39c9f8
hassan848 <72468804+hassan848@users.noreply.github.com>
2021-01-10 15:17:42 +0000

    Add files via upload

---
+++ Python_API.py
@@ -0,0 +1,17 @@
+import paramiko
+import getpass
+
+host = input('Enter host IP')
+port = 22
+username = input("Enter your remote account: ")
+password = getpass.getpass()
+
+command = "ls"
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, username, password)
+
+stdin, stdout, stderr = ssh.exec_command(command)
+for l in stdout.readlines():
+print(l)
