dc9e3476237582571db3f08f0bd33183d9cefb21
Hassan <hassanaslam786@hotmail.co.uk>
2021-01-15 21:32:04 +0000

    Python API which executes shell script remotely to other VMs

--- Python_API.py
+++ Python_API.py
@@ -1,17 +1,48 @@
 import paramiko
 import getpass
+from datetime import datetime
 
-host = input('Enter host IP: ')
+filename = str(input("Please Enter a Shell Filename: "))
+print(filename)
+
+f = open(f"{filename}", "r")
+rf = f.read()
+
 port = 22
-username = input("Enter your remote account: ")
-password = getpass.getpass()
 
-command = "ls"
+def getWorkerInfo():
+	host = input('Enter host IP: ')
+	username = input('Enter your remote account: ')
+	password = getpass.getpass()
+	return {'host': host, 'name': username, 'password': password}
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(host, port, username, password)
 
-stdin, stdout, stderr = ssh.exec_command(command)
-for l in stdout.readlines():
-	print(l)
+command = [f"sh {filename}"]
+
+def ssh_connection(host, username, password):
+	ssh = paramiko.SSHClient()
+	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	ssh.connect(host, port, username, password)
+	#now we need to transfer the entire shell file
+	sftp_client=ssh.open_sftp()
+
+	sftp_client.put(f"{filename}", f"{filename}")
+	print()
+	startTime = datetime.now()
+
+	stdin, stdout, stderr = ssh.exec_command(command[0])
+	endTime = (datetime.now() - startTime)
+	for l in stdout.readlines():
+		print(l.strip()) #strip() will print the result without any line breaks
+
+	sftp_client.close()
+	ssh.close()
+	print(f"The time to execute this task was: {endTime}")
+
+print()
+worker1 = getWorkerInfo()
+print()
+worker2 = getWorkerInfo()
+
+ssh_connection(worker1['host'], worker1['name'],worker1['password'])
+ssh_connection(worker2['host'], worker2['name'],worker2['password'])
